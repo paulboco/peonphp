e5a7bbe2d5839b66063eec5e328093e59d92e2d9
paulboco <paulboco@cableone.net>
2015-10-02 12:53:04 -0500

    Add .gitignore and run composer install.

--- bootstrap/framework.php
+++ bootstrap/framework.php
@@ -2,6 +2,32 @@
 
 /*
 |--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| our application. We just need to utilize it! We'll simply require it
+| into the script here so that we don't have to worry about manual
+| loading any of our classes later on. It feels nice to relax.
+|
+*/
+
+require __DIR__.'/../vendor/autoload.php';
+
+
+
+$router = New Router;
+die;
+
+
+
+
+
+
+
+
+/*
+|--------------------------------------------------------------------------
 | Include Framework Services
 |--------------------------------------------------------------------------
 |
