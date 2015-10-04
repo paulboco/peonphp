89fd24b88bdd224cf7c1bb84ac8d67c7497ce998
paulboco <paulboco@cableone.net>
2015-10-04 16:46:29 -0500

    Add database sub directories.
    Signed-off-by: paulboco <paulboco@cableone.net>

---
+++ database/schema/prisoners.php
@@ -0,0 +1,7 @@
+<?php
+
+return "CREATE TABLE `prisoners` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
+    `name` VARCHAR(100) NOT NULL ,
+    PRIMARY KEY (`id`))
+    ENGINE = InnoDB;";
--- script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "alias ll='ls -al'\n" > ~/home/vagrant/.bash_aliases
-echo "alias c=clear\n" >> ~/home/vagrant/.bash_aliases
