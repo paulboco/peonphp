84ed41c8f568c864501062b8e291942c5cf53e59
paulboco <paulboco@cableone.net>
2015-10-20 14:50:25 -0500

    Delete API files.

--- public/api/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["f","config()"],["f","d()"],["f","dd()"],["f","e()"],["f","path()"],["c","Peon\\App"],["c","Peon\\Config"],["c","Peon\\Container"],["c","Peon\\Controller"],["c","Peon\\Gopher"],["c","Peon\\Request"],["c","Peon\\Router"],["c","Peon\\View"],["f","redirect()"],["f","segment()"]];
