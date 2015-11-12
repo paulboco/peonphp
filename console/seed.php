0e2de0276ec002d72f7728091cc733a46dead606
paulboco <paulboco@cableone.net>
2015-11-11 20:54:17 -0600

    Fix typo.

--- console/seed.php
+++ console/seed.php
@@ -1,8 +1,8 @@
 <?php
 
+$database = 'peon';
 $username = getenv('DB_USER');
 $password = getenv('DB_PASS');
-$database = getenv('DB_NAME');
 
 if ($argc < 1) {
     $this->fatal('Not enough arguments');
@@ -16,8 +16,8 @@ if (!extension_loaded('mysql')) {
     $this->fatal('MySql is not loaded');
 }
 
-exec("mysql -u{$username} -p{$password} -e \"DROP DATABASE IF EXISTS {$database}\";", $output, $return1);
-exec("mysql -u{$username} -p{$password} -e \"CREATE DATABASE IF NOT EXISTS {$database}\";", $output, $return2);
+exec("mysql -u{$username} -p{$password} -e 'DROP DATABASE IF EXISTS {$database}';", $output, $return1);
+exec("mysql -u{$username} -p{$password} -e 'CREATE DATABASE IF NOT EXISTS {$database}';", $output, $return2);
 exec("mysql -u{$username} -p{$password} {$database} < ./database/mysql/{$database}.sql", $output, $return3);
 
 if ($return1 or $return2 or $return3) {
